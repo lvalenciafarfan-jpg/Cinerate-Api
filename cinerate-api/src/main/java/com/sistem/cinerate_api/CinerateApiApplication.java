0bd60a75314623dafac3613f0751b8369955e34c
Luis Valencia <lvalenciafarfan@gmail.com>
2026-06-01 20:43:36 -0500

    first commit

---
+++ cinerate-api/src/main/java/com/sistem/cinerate_api/CinerateApiApplication.java
@@ -0,0 +1,13 @@
+package com.sistem.cinerate_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CinerateApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CinerateApiApplication.class, args);
+	}
+
+}
---
+++ cinerate-api/src/test/java/com/sistem/cinerate_api/CinerateApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sistem.cinerate_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CinerateApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
